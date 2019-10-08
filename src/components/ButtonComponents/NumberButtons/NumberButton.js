1ff9367e1cf3415b8ef7d1b2a15d5bdf0120190b
ngaivoronski <ngaivoronski@gmail.com>
2019-10-08 18:44:13 -0400

    button setup and digit entry

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,22 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
+  function enterNumber(num) {
+    if (parseInt(num) === 0) {
+      return (props.number);
+    } else {
+      return (num + props.number);
+    }
+  }
+
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button onClick={() => props.setValue(enterNumber(props.value))}>
+        {props.number}
+      </button>
     </>
   );
 };
+
+export default NumberButton;
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,19 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
-import { numbers } from '../../../data' 
-*/
-//Import your array data to from the provided data file
+import NumberButton from "./NumberButton";
 
-const Numbers = () => {
+//Import your array data to from the provided data file
+import { numbers } from '../../../data';
+
+export default function Numbers(props) {
   // STEP 2 - add the imported data to state
+  const [numberState] = useState(numbers);
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+      component matching the name on the provided file. Pass
+      it any props needed by the child component*/}
+      {numberState.map((number) => (
+        <NumberButton number={number} setValue={props.setValue} value={props.value} />
+      ))}
     </div>
   );
 };
